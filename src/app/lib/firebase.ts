eac5877c9282e5f062c7d4a1355e1e4ba247725d
iamrosada0 <luisrosada@outlook.com>
2025-07-10 23:47:43 +0100

    feat: create firebase module for authentication with Google

---
+++ src/app/lib/firebase.ts
@@ -0,0 +1,18 @@
+// src/lib/firebase.ts
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  /* keys */
+};
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+
+export const loginWithGoogle = () => signInWithPopup(auth, googleProvider);
+export const logout = () => signOut(auth);
