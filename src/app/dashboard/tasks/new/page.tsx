25d6f68a0939c9724e40371ad35798294e060b14
iamrosada0 <luisrosada@outlook.com>
2025-07-13 22:54:39 +0100

    feat: add Suspense

--- src/app/dashboard/tasks/new/page.tsx
+++ src/app/dashboard/tasks/new/page.tsx
@@ -1,7 +1,10 @@
-"use client";
-
+import { Suspense } from "react";
 import NewTaskForm from "@/components/task/create/NewTaskForm";
 
 export default function NewTaskPage() {
-  return <NewTaskForm />;
+  return (
+    <Suspense fallback={<div>Carregando formulário...</div>}>
+      <NewTaskForm />
+    </Suspense>
+  );
 }
